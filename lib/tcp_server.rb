59795a3d5f906baa2912ac9cf833b1487e63e0fc
LeonardoNTI <leonardo.giannini@elev.ga.ntig.se>
2025-02-17 08:47:09 +0100

    Lite knas

--- lib/tcp_server.rb
+++ lib/tcp_server.rb
@@ -15,22 +15,15 @@ class HTTPServer
 
     # Initialize Router and Add Routes
     router = Router.new
-    router.add_route(:get, '/') { erb('index') }
-    router.add_route(:get, '/logged_in') { erb('logged_in') }
-    router.add_route(:get, '/about') { erb('about')}
+    router.add_route(:get, '/') { erb('index') }  # FIXED
+    router.add_route(:get, '/about') { erb('about') }
     router.add_route(:get, '/login') { erb('login') }
-    router.add_route(:get, '/users/:id') do |request|
-      # Här genererar vi själva HTML-innehållet
-      "<h1>User Profile</h1><p>Welcome, user with ID: #{request.params[:id]}</p><a href='/'>Go back to Home</a>"
-    end
-
     router.add_route(:post, '/submit') { { redirect: '/logged_in' } }
-
     router.add_route(:get, '/add/:num1/:num2') do |request|
-      num1 = request.params[:num1].to_i 
+      num1 = request.params[:num1].to_i
       num2 = request.params[:num2].to_i
       result = num1 + num2
-      "<h1>Resultat: #{num1} + #{num2} = #{result}</h1>"
+      "<h1>Result: #{num1} + #{num2} = #{result}</h1>"
     end
 
     while session = server.accept
@@ -44,7 +37,6 @@ class HTTPServer
       puts "-" * 40
 
       request = Request.new(data)
-
       route = router.match_route(request)
 
       session.print route.to_s  # Send the response to the client
@@ -52,27 +44,27 @@ class HTTPServer
     end
   end
 
-  # Förenklad erb-metod som läser in vyfil och layout
+  # Simplified ERB rendering method
   def erb(view_file, use_layout = true)
     view_path = "../views/#{view_file}.erb"
     layout_path = "../views/layout.erb"
 
-    # Läs in vyfilen
+    # Read view file
     begin
       view_content = File.read(view_path)
-    rescue Errno::ENOENT => e
+    rescue Errno::ENOENT
       return "Error: View file #{view_file} not found."
     end
 
-    # Om layout är påslagen, läs in layoutfilen
+    # Read layout if enabled
     if use_layout
       begin
         layout_content = File.read(layout_path)
-      rescue Errno::ENOENT => e
+      rescue Errno::ENOENT
         return "Error: Layout file not found."
       end
 
-      # Sätt in vyinnehållet i layoutens yield-område
+      # Insert view content into layout
       layout_content.gsub('<%= yield %>', view_content)
     else
       view_content
@@ -80,6 +72,6 @@ class HTTPServer
   end
 end
 
-# Skapa och starta servern
+# Start the server
 server = HTTPServer.new(4567)
 server.start
